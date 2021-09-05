f9d813f20dd4a36aafb3702777d32083aa9c42f4
ColinTing <honey.hao.2012@gmail.com>
2021-09-05 16:11:57 +0800

    创建multi-branch-step-0分支，用于存储变量totalAmount还未被getTotalCharge()取代时的状态
    创建multi-branch-step-1分支，用于存储getTotalCharge()取代变量totalAmount后的代码

--- src/main/java/com/colinting/refactoring/videostore/Customer.java
+++ src/main/java/com/colinting/refactoring/videostore/Customer.java
@@ -28,25 +28,32 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
-        String result ="com.colinting.refactoring.videostore.Rental Record for " + getName() + "\n";
+        String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints +=
-                    each.getFrequentRenterPoints();
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
                     String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
+
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) +
-                " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints)
+                + " frequent renter points";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
 }
