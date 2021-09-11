b1fb96649cd64b1f44d8d56ec378f48e4afb9b8b
ColinTing <honey.hao.2012@gmail.com>
2021-09-11 15:38:40 +0800

    Replace Temp with Query 'frequentRenterPoints'

--- src/main/java/com/colinting/refactoring/videostore/Customer.java
+++ src/main/java/com/colinting/refactoring/videostore/Customer.java
@@ -28,12 +28,10 @@ public class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
@@ -42,8 +40,18 @@ public class Customer {
 
         // add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints)
+        result += "You earned " + String.valueOf(getFrequentRenterPoints())
                 + " frequent renter points";
+        return result;
+    }
+
+    private int getFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
         return result;
     }
 
