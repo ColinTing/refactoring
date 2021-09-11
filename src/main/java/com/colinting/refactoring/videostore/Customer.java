d1d0f36c56c00d15304da6ab6c9c694dec798f91
ColinTing <honey.hao.2012@gmail.com>
2021-09-11 16:11:27 +0800

    htmlStatement()

--- src/main/java/com/colinting/refactoring/videostore/Customer.java
+++ src/main/java/com/colinting/refactoring/videostore/Customer.java
@@ -40,12 +40,31 @@ public class Customer {
 
         // add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getFrequentRenterPoints())
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints())
                 + " frequent renter points";
         return result;
     }
 
-    private int getFrequentRenterPoints() {
+
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for each rental
+            result += each.getMovie().getTitle() + ": " +
+                    String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        // add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) +
+                "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+                String.valueOf(getTotalFrequentRenterPoints())
+                + "</EM> frequent renter points<P>";
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
         int result = 0;
         Enumeration rentals = _rentals.elements();
         while (rentals.hasMoreElements()) {
