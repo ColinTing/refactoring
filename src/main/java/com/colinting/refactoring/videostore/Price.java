af2d8382ba0589a13fa3215f3d2c3eab947e55d9
ColinTing <honey.hao.2012@gmail.com>
2021-09-12 21:20:52 +0800

    Replace Conditional with Polymorphism(255) 1.条件替换成多态 这样就不用通过priceCode来判断执行语句而是通过实体类来判断执行的运算语句

--- src/main/java/com/colinting/refactoring/videostore/Price.java
+++ src/main/java/com/colinting/refactoring/videostore/Price.java
@@ -8,12 +8,23 @@ package com.colinting.refactoring.videostore;
 
 public abstract class Price {
     abstract int getPriceCode();
+
+    abstract double getCharge(int daysRented);
 }
 
 class ChildrensPrice extends Price {
     @Override
     int getPriceCode() {
         return Movie.CHILDRENS;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3) {
+            result += (daysRented - 3) * 1.5;
+        }
+        return result;
     }
 }
 
@@ -22,11 +33,25 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
 
 class RegularPrice extends Price {
     @Override
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result =  2;
+        if (daysRented > 2) {
+            result += (daysRented - 2) * 1.5;
+        }
+        return result;
     }
 }
